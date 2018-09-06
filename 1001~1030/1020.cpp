b0f02418ec95bfb50b4a2d9ecbe82c68126d49c6
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-06 20:36:56 +0800

    Update 1020.cpp

--- 1001~1030/1020.cpp
+++ 1001~1030/1020.cpp
@@ -1,67 +1,39 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
 
 int N;
-
 vector<int>post, in;
-
 vector<vector<int> >level;
 
 
-void levelorder(int postr, int inl, int inr, int h){
-   
-    if(inl > inr)   return ;
-   
+void levelorder(int postr, int inl, int inr, int h){ 
+    if(inl > inr)   return ; 
     int i = inl;
-   
     while(in[i] != post[postr]) i++;
-
     level[h].push_back( in[i] );
-
     levelorder(i-inr+postr-1, inl, i-1, h+1);
-
     levelorder(postr-1, i+1, inr, h+1);
-
 }
 
 
-
-void print(){
-   
-   for(int h = 0;; h++){
-      
+void print(){ 
+   for(int h = 0;; h++){   
        for(int i = 0; i < level[h].size(); i++){
-
          cout<<level[h][i];
-
          if(--N) cout<<" ";
-
          else return;
-
       }
-
    }
-
 }
 
 
-
-int main(){
-   
+int main(){ 
    cin >> N;
-
    post.resize(N), in.resize(N), level.resize(N);
-
    for(int i = 0; i < N; i++) cin >> post[i];
-
    for(int i = 0; i < N; i++) cin >> in[i];
-
    levelorder(N-1, 0, N-1, 0);
-
    print();
-
    return 0;
-
 }
