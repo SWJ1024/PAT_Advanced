6fb827c5e1df63b2dfbf54bcfa3b0586380a794c
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-06 20:34:32 +0800

    Update 1011.cpp

--- 1001~1030/1011.cpp
+++ 1001~1030/1011.cpp
@@ -1,39 +1,20 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-
-
 int main(){
-
    int T = 3;
-
    double sum = 1, a, b, c;
-
    while(T--){
-
      cin >> a >> b >> c;
-
      if(a > b){
-
          if(a > c) {cout << "W "; sum *= a;}
-
          else      {cout << "L "; sum *= c;}
- 
      }
-
      else{
-
          if(b > c) {cout << "T "; sum *= b;}
-
          else      {cout << "L "; sum *= c;}
- 
      }
-
     }
-
     printf("%.2f\n",(0.65*sum - 1)*2);
-
     return 0;
-
 }
