daa929964148613a2d2a3c971ccefaf24886d6b3
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-06 20:38:58 +0800

    Update 1027.cpp

--- 1001~1030/1027.cpp
+++ 1001~1030/1027.cpp
@@ -1,37 +1,19 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-
-
 void MarColor(int n){
-
    if(n < 10) cout<<n;
-
    else cout << char(n-10+'A');
-
    return;
-
 }
 
-
-
 int main(){
-
    int T = 3, n;
-
    cout << '#';
-
    while(T--){
-
       cin >> n;
-
       MarColor( n/13 );
-
       MarColor( n%13 );
-
    }
-
    return 0;
-
 }
