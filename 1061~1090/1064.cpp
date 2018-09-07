ddb5fdf2db917b1a98bcb16e9f767da50c91493b
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-07 08:57:07 +0800

    Update 1064.cpp

--- 1061~1090/1064.cpp
+++ 1061~1090/1064.cpp
@@ -6,32 +6,32 @@ vector< int > pre;
 vector< vector< int > > H(15);
 
 int Log2(int x){
-	return log(x)/log(2)-0.00005;
+   return log(x)/log(2)-0.00005;
 }
 
 
 int LeftNum(int N){
-   	int h = Log2(N+1)+1;
-  	int n = pow(2, h-2)-1+0.5;
-  	int Left = N - 2*n - 1;
-  	return n+min(Left, (int)(pow(2, h-2)+0.5) );
+   int h = Log2(N+1)+1;
+   int n = pow(2, h-2)-1+0.5;
+   int Left = N - 2*n - 1;
+   return n+min(Left, (int)(pow(2, h-2)+0.5) );
 }
 
 void levelorder(int prel, int prer, int height){
-	if(prer < prel) return;
-	int n = prer-prel+1, L = LeftNum(n), R = n-L;
-	H[height].push_back( pre[prel+L] );
-	levelorder(prel, prel+L-1, height+1);
-	levelorder(prel+L+1,prer, height+1);
+   if(prer < prel) return;
+   int n = prer-prel+1, L = LeftNum(n), R = n-L;
+   H[height].push_back( pre[prel+L] );
+   levelorder(prel, prel+L-1, height+1);
+   levelorder(prel+L+1,prer, height+1);
 }
 
 void print(){
    int cnt = 0, i = 0;
    do{
-   	  for(auto &j : H[i]){
-   	  	 printf("%d", j);
-   	  	 if(++cnt == N) return;
-   	  	 else putchar(' ');
+      for(auto &j : H[i]){
+   	 printf("%d", j);
+   	 if(++cnt == N) return;
+   	 else putchar(' ');
       }
   }while(++i);
 }
