3da8a31822d89d1d1e6a0deccce50cb50e60b0b5
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-07 09:06:00 +0800

    Update 1115.cpp

--- 1091~1120/1115.cpp
+++ 1091~1120/1115.cpp
@@ -9,33 +9,31 @@ bool book[MAXN][2];
 
 
 void build(int i, int id, int x, int deep){
-	maxdeep = max( deep, maxdeep );
-	height[deep]++;
-	if(x <= v[i]){
-   		if( !book[i][0] ){
-   			book[i][0] = true;
-   			T[i][0] = id; 
-   		}
-   		else build( T[i][0], id, x, deep+1);
+   maxdeep = max( deep, maxdeep );
+   height[deep]++;
+   if(x <= v[i]){
+   	if( !book[i][0] ){
+   	   book[i][0] = true;
+   	   T[i][0] = id; 
    	}
-   	else{
-   	   if( !book[i][1] ){
-   			book[i][1] = true;
-   			T[i][1] = id; 
-   		}
-   		else build( T[i][1], id, x, deep+1);
+   	else build( T[i][0], id, x, deep+1);
+   }
+   else{
+        if( !book[i][1] ){
+   	   book[i][1] = true;
+   	   T[i][1] = id; 
    	}
+   	else build( T[i][1], id, x, deep+1);
+   }
 }
-
-
 
 
 int main(){
    scanf("%d", &N);
    v.resize(N);
    for(int i = 0; i < N; i++){
-   	  scanf("%d", &v[i]);
-   	  if(i) build(0, i, v[i], 0);
+       scanf("%d", &v[i]);
+       if(i) build(0, i, v[i], 0);
    }
    if(N == 1) cout << "1 + 0 = 1\n";
    else printf("%d + %d = %d\n", height[maxdeep], height[maxdeep-1]-height[maxdeep], height[maxdeep-1]);
