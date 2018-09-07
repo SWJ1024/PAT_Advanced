0beb49152a75bad10e0e46479c8c4568da14a0d9
a good loser <32921429+874288174@users.noreply.github.com>
2018-09-07 10:30:02 +0800

    Add files via upload

---
+++ 1091~1120/1098.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> a, b;
+int n;
+
+void downAdjust(int low, int high) {
+    int i = 1, j = i * 2;
+    while(j <= high) {
+        if(j + 1 <= high && b[j] < b[j + 1]) j++;
+        if(b[i] < b[j]) {
+            swap(b[i], b[j]);
+            i = j;   j = i * 2;
+        } 
+       else break;
+    }
+}
+
+
+void print(){
+   for(int i = 1; i <= n; i++)
+      printf("%d%c", b[i], (i == n ? '\n' : ' '));
+}
+
+int main() {
+    int p = 2;
+    scanf("%d", &n);
+    a.resize(n + 1), b.resize(n + 1);
+    for(int i = 1; i <= n; i++)  scanf("%d", &a[i]);
+    for(int i = 1; i <= n; i++)  scanf("%d", &b[i]);
+    while(p <= n && b[p - 1] <= b[p]) p++;
+    int index = p;
+    while(p <= n && a[p] == b[p]) p++;
+    if(p == n + 1) {
+        printf("Insertion Sort\n");
+        sort(b.begin() + 1, b.begin() + index + 1);
+        print();
+    }
+    else {
+        printf("Heap Sort\n");
+        p = n;
+        while(p >= 2 && b[p] >= b[p - 1]) p--;
+        swap(b[1], b[p]);
+        downAdjust(1, p - 1);
+        print();
+    }
+    return 0;
+}
